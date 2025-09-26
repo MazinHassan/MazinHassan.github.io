9b8f250c85261a88f968aa635b2ebfc4b4c5bffb
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-09-26 10:50:05 +0300

    init

---
+++ data.js
@@ -0,0 +1,266 @@
+const data = {
+  title: "Mazin Hassan",
+  hero: {
+    first_name: "Mazin",
+    last_name: "Hassan",
+    description:
+      "Freelance Illustrator Concept Artist Character Designer Storyboard Artist",
+    image: "./gallery/ddddddddddoodles.png",
+    contacts: [
+      {
+        link: "https://twitter.com/RinkaRii_",
+        body: '<i class="fa-brands fa-twitter"></i>',
+      },
+      {
+        link: "https://www.instagram.com/rinkariiii/",
+        body: '<i class="fa-brands fa-instagram"></i>',
+      },
+      {
+        link: "mailto:rinkariiii@gmail.com",
+        body: '<i class="fa-solid fa-envelope"></i>',
+      },
+    ],
+  },
+  gallery: [
+    {
+      media: "./gallery/biscuit commish.png",
+      description: "",
+    },
+    {
+      media: "./gallery/afaaefa.PNG",
+      description: "",
+    },
+    {
+      media: "./gallery/commish 2.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/COMMISH.png",
+      description: "",
+    },
+    {
+      media: "./gallery/commish sheet.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/commish takeo.png",
+      description: "",
+    },
+    {
+      media: "./gallery/commision.png",
+      description: "",
+    },
+    {
+      media: "./gallery/conrad.png",
+      description: "",
+    },
+    {
+      media: "./gallery/cyber rinka.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/D7mvrRGWwAYtjly.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/D7xOxhoXsAIoc-2.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/ddddddddddoodles.png",
+      description: "",
+    },
+    {
+      media: "./gallery/doodle.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/dsfdsfsdfsdfs.png",
+      description: "",
+    },
+    {
+      media: "./gallery/duck.png",
+      description: "",
+    },
+    {
+      media: "./gallery/dumdums.png",
+      description: "",
+    },
+    {
+      media: "./gallery/edited.png",
+      description: "",
+    },
+    {
+      media: "./gallery/ferrigga.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/ffffffffffff.PNG",
+      description: "",
+    },
+    {
+      media: "./gallery/fghrftghfgtdhdf.PNG",
+      description: "",
+    },
+    {
+      media: "./gallery/firehorsebunny1.png",
+      description: "",
+    },
+    {
+      media: "./gallery/fisheye.png",
+      description: "",
+    },
+    {
+      media: "./gallery/FPu8rd9X0AAXto0.jfif",
+      description: "",
+    },
+    {
+      media: "./gallery/FPyNFyGXsCQH4s0.jfif",
+      description: "",
+    },
+    {
+      media: "./gallery/furry commish.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/ggggggiel.png",
+      description: "",
+    },
+    {
+      media: "./gallery/GO SKATE DAY POSTER.png",
+      description: "",
+    },
+    {
+      media: "./gallery/hanabi model.png",
+      description: "",
+    },
+    {
+      media: "./gallery/human.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/Illustration5.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/jhcjycjuy.png",
+      description: "",
+    },
+    {
+      media: "./gallery/lilooo.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/lost kitten_Trim.mp4",
+      description: "",
+    },
+    {
+      media: "./gallery/luke mom.png",
+      description: "",
+    },
+    {
+      media: "./gallery/luke.png",
+      description: "",
+    },
+    {
+      media: "./gallery/lukes wish.png",
+      description: "",
+    },
+    {
+      media: "./gallery/mech arm zine.png",
+      description: "",
+    },
+    {
+      media: "./gallery/miku commish.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/oiszhfdids.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/o.jfif",
+      description: "",
+    },
+    {
+      media: "./gallery/oo.png",
+      description: "",
+    },
+    {
+      media: "./gallery/pasldsadsadsadsadasdsadasdsadsadsadasd.png",
+      description: "",
+    },
+    {
+      media: "./gallery/perdita postwer.png",
+      description: "",
+    },
+    {
+      media: "./gallery/Ray The rouge tiefling.png",
+      description: "",
+    },
+    {
+      media: "./gallery/romaaaaaa.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/sailor moon.png",
+      description: "",
+    },
+    {
+      media: "./gallery/scott pilgrim.png",
+      description: "",
+    },
+    {
+      media: "./gallery/sfsfs.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/skelly.jpg",
+      description: "",
+    },
+    {
+      media: "./gallery/something.png",
+      description: "",
+    },
+    {
+      media: "./gallery/tetfw.png",
+      description: "",
+    },
+    {
+      media: "./gallery/thing.png",
+      description: "",
+    },
+    {
+      media: "./gallery/val.png",
+      description: "",
+    },
+    {
+      media: "./gallery/wsfss.png",
+      description: "",
+    },
+    {
+      media: "./gallery/xdd.png",
+      description: "",
+    },
+    {
+      media: "./gallery/yeahhh.png",
+      description: "",
+    },
+    {
+      media: "./gallery/yes (2).png",
+      description: "",
+    },
+    {
+      media: "./gallery/yes.png",
+      description: "",
+    },
+    {
+      media: "./gallery/سيبهيسبلايسلاسيهب.png",
+      description: "",
+    },
+    {
+      media: "./gallery/صثبخثب.png",
+      description: "",
+    },
+  ],
+};
---
+++ script.js
@@ -0,0 +1,62 @@
+document.title = data.title;
+
+const hero = document.querySelector(".hero");
+const heroContent = document.querySelector(".hero-content");
+const heroName = document.querySelector(".hero-name");
+const heroDescription = document.querySelector(".hero-description");
+const heroContacts = document.querySelector(".hero-contacts");
+const gallery = document.querySelector(".gallery");
+
+hero.style.backgroundImage = `url('${data.hero.image}')`;
+heroName.innerHTML = `${data.hero.first_name}</br>${data.hero.last_name}`;
+heroDescription.innerHTML = data.hero.description;
+heroContacts.innerHTML = data.hero.contacts
+  .map((c) => `<a href="${c.link}">${c.body}</a>`)
+  .join("");
+
+const heroObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        heroContent.style.color = "black";
+      } else {
+        heroContent.style.color = "white";
+      }
+    });
+  },
+  { threshold: 0.1 },
+);
+
+heroObserver.observe(hero);
+
+gallery.innerHTML += data.gallery
+  .map((i) => {
+    const isVideo = /\.(mp4|mov|avi|webm|mkv)$/i.test(i.media);
+    const media = isVideo
+      ? `<video class="gallery-media" onmouseenter="this.setAttribute('controls', 'controls')" onmouseleave="this.removeAttribute('controls')"><source src="${i.media}" onload="this.classList.remove('loading')"></video>`
+      : `<img class="gallery-media" src="${i.media}" loading="lazy" />`;
+
+    return `
+				<div class="gallery-item">
+					${media}
+					<p>${i.description}</p>
+				</div>
+			`;
+  })
+  .join("");
+
+const loadingObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1";
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.1 },
+);
+
+for (const child of gallery.children) {
+  loadingObserver.observe(child);
+}
